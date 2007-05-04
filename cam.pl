9aaf9ec02368bb041792ad0e216906a2f6681aee
Nicola Worthington <nicolaw@tfb.net>
2007-05-04 14:42:27 +0000

    git-svn-id: file:///var/tmp/data/svnroot/camd@1022 610ec7d1-1c12-4784-ad90-13d12571cf29

--- cam.pl
+++ cam.pl
@@ -1,5 +1,41 @@
 #!/home/nicolaw/perl-5.8.8/bin/perl -w
 
+use constant COPYRIGHT         => sprintf('Copyright (c)%04d Nicola Worthington. All rights reserved.', (localtime(time))[5]+1900);
+use constant IMAGE_FORMAT      => 'jpg';
+use constant OVERLAY_IMAGE     => 'cam.png';
+
+use constant ANNOTATE_IMAGE    => 1;
+use constant ADD_TIMESTAMP     => 1;
+
+use constant CAPTION_NOVIEWERS => 'PAUSED - PLEASE WAIT A MOMENT';
+use constant CAPTION_FILE      => '/tmp/caption';
+use constant CAPTION_FONT      => 'Silkscreen.ttf';
+use constant CAPTION_COLOUR    => '#ffffff';
+use constant CAPTION_BG_COLOUR => '#898E79';
+
+use constant DEFAULT => {
+		SOAP_SERVER_PORT  => 8021,               # SOAP server port
+		SOAP_SERVER       => 'www.neechi.co.uk', # SOAP server hostname/IP
+		SOAP_KEY          => 'ec2b5a007a8d0431a36ecadc815e9d82', # SOAP server key
+		WEBCAM_HOST       => 'webcam',           # Webcam hostname/IP
+		WEBCAM_PORT       => 80,                 # Webcam port
+		IMAGE_RESOLUTION  => '352x288',          # Resolution
+		IMAGE_COMPRESSION => 30,                 # Compression %
+		UPLOAD_FREQUENCY  => 0.05,               # Upload frequency
+		CAPTION           => '',                 # Caption/message
+		CROPPING          => undef,              # Crop
+	};
+
+
+##############################################################
+#
+#
+#         NO USER SERVICABLE PARTS BEYOND THIS POINT!
+#
+#
+##############################################################
+
+
 use strict;
 use threads;
 use Thread::Queue;
@@ -7,14 +43,38 @@ use Proc::DaemonLite;
 use Getopt::Std qw();
 use POSIX qw(strftime);
 
-my $opts = {};
-Getopt::Std::getopts('hvdr:w:c:C:p:s:k:m:', $opts);
+# The following modules are loaded after this process has created
+# all of its worker threads. This ensures that we don't have every
+# module loaded in every thread, which saves a lot of memory.
+#      Image::Magick
+#      LWP::Simple
+#      Time::HiRes
+#      SOAP::Lite
+
+# Default command line options
+my $opts = {
+		p => DEFAULT->{SOAP_SERVER_PORT},
+		s => DEFAULT->{SOAP_SERVER},
+		k => DEFAULT->{SOAP_KEY},
+		w => DEFAULT->{WEBCAM_HOST},
+		P => DEFAULT->{WEBCAM_PORT},
+		r => DEFAULT->{IMAGE_RESOLUTION},
+		c => DEFAULT->{IMAGE_COMPRESSION},
+		f => DEFAULT->{UPLOAD_FREQUENCY},
+		m => DEFAULT->{CAPTION},
+		C => DEFAULT->{CROPPING},
+	};
+
+# Parse command line options
+Getopt::Std::getopts('hvdr:w:c:C:p:s:k:m:f:', $opts);
 printf("%s %s\n",$0,'$Id$'), exit if defined $opts->{v};
 printf("Syntax: %s [-h|-v] [-d] [-m <freeform message>]
         [-w <webcam host>] [-r <resolution>] [-c <compression>]
-        [-s <server host>] [-p <port>] [-k <key>]\n", $0), exit if defined $opts->{h};
+        [-s <server host>] [-p <port>] [-k <key>]
+        [-f <upload frequency>]\n", $0), exit if defined $opts->{h};
 
-init_server() if defined $opts->{d};
+# Daemonise
+Proc::DaemonLite::init_server() if defined $opts->{d};
 
 $| = 1;
 
@@ -22,6 +82,7 @@ my %threads = ();
 my $imgq : shared = new Thread::Queue;
 my $viewers : shared = 0;
 
+# Create the worker threads
 for my $sub (qw(GetImage SendImage)) {
 	my $cref = eval("\\&$sub");
 	print "Creating $sub thread ...\n";
@@ -34,127 +95,138 @@ for my $thread (threads->list) {
 
 exit;
 
+
+
+##############################
+# Thread sub routines
+
 sub GetImage {
 	my ($sub,$opts) = @_;
 	require LWP::Simple;
 	require Time::HiRes;
 
-	my $sleep_wait = 0.05;
-	my $host = $opts->{w} || 'webcam.tfb.net';
-	my $resolution = $opts->{r} || '352x288';
-	my $compression = $opts->{c} || 30;
-	my $crop = $opts->{C} || '200x200+552+488';
-	my $port = $opts->{P} || 80;
 	my $imgurl = sprintf('http://%s:%d/axis-cgi/jpg/image.cgi?resolution=%s&compression=%d&date=0&text=0&showlength=1',
-					$host, $port, '640x480', 0);
+					$opts->{w}, $opts->{P}, '640x480', 0);
 
 	for (;;) {
 		if ($imgq->pending < 2) {
 			eval {
-				my $img = ProcessImage(LWP::Simple::get($imgurl),
-						resolution => $resolution,
-						compression => $compression,
-						crop => $crop,
-					);
+				my $img = ProcessImage(LWP::Simple::get($imgurl), $opts);
 				$imgq->enqueue($img);
 			};
 			print $@ ? '{' : '<';
 		} else {
-			Time::HiRes::sleep($sleep_wait);
+			Time::HiRes::sleep($opts->{f});
 		}
 	}
 }
 
+
 sub ProcessImage {
-	my $img = shift;
-	my $opt = { @_ };
+	my ($img,$opts) = @_;
 	require Image::Magick;
+	require FindBin;
 
-	my $msg = `cat /tmp/caption 2>/dev/null` || '';
-	$msg = 'PAUSED - WAIT 10 SECONDS' if !$viewers;
+	my $caption_file = CAPTION_FILE;
+	my $msg = $opts->{m};
+	if (-f $caption_file && open(MSG,'<',$caption_file)) {
+		$msg = <MSG>; chomp $msg;
+		close(MSG);
+	}
+	$msg = CAPTION_NOVIEWERS if !$viewers;
 
-	my $quality = defined $opt->{compression} ? 100 - $opt->{compression} : 70;
-	my ($width,$height) = defined $opt->{resolution} ? split(/\D+/,$opt->{resolution}) : (352,288);
+	my $font = "$FindBin::Bin/".CAPTION_FONT;
+	my $quality = defined $opts->{c} ? 100 - $opts->{c} : 70;
+	my ($width,$height) = defined $opts->{r} ? split(/\D+/,$opts->{r}) : (352,288);
 
-	my $image = Image::Magick->new(magick => 'jpg');
+	my $image = Image::Magick->new(magick => IMAGE_FORMAT);
 	$image->BlobToImage($img);
 	$image->Set(quality => $quality);
 	$image->Set(type => "TrueColorMatte");
 
-	if (defined $opt->{crop}) {
+	if (defined $opts->{C} && $opts->{C}) {
+		# TODO
+		# This is not extracting the information from
+		# $opts->{C} yet when it should be.
 		$image->Crop(x=>130, y=>100, width => 480, height=>360);
 	}
 	$image->Resize(width => $width, height => $height);
 
-	$image->Comment(sprintf('Copyright (c)%04d Nicola Worthington. All rights reserved.', (localtime(time))[5]+1900) );
-# http://studio.imagemagick.org/pipermail/magick-users/2003-June/009442.html
+	$image->Comment(COPYRIGHT);
+	# http://studio.imagemagick.org/pipermail/magick-users/2003-June/009442.html
 
-	$image->Draw(
-			stroke => '#666666',
-			fill => '#ffffff',
-			primitive => 'rectangle',
-			points => sprintf('%d,%d %d,%d',0,0,$width-1,14),
-		);
+	if (ANNOTATE_IMAGE) {
+		# Draw annotation background
+		$image->Draw(
+				stroke    => '#666666',
+				fill      => CAPTION_COLOUR,
+				primitive => 'rectangle',
+				points    => sprintf('%d,%d %d,%d',0,0,$width-1,14),
+			);
+		$image->Draw(
+				stroke    => CAPTION_BG_COLOUR,
+				fill      => CAPTION_BG_COLOUR,
+				primitive => 'rectangle',
+				points    => sprintf('%d,%d %d,%d',81,2,$width-3,12),
+			);
 
-	$image->Draw(
-			stroke => '#898E79',
-			fill => '#898E79',
-			primitive => 'rectangle',
-			points => sprintf('%d,%d %d,%d',81,2,$width-3,12),
-		);
+		# Add timestamp
+		$image->Annotate(
+				font      => $font,
+				pointsize => 8,
+				fill      => CAPTION_COLOUR,
+				text      => strftime('%Y-%m-%d %H:%M:%S',localtime),
+				x         => 85,
+				y         => 10,
+			) if ADD_TIMESTAMP;
 
-	$image->Annotate(
-			font => '/home/nicolaw/bin/Silkscreen.ttf',
-			pointsize => 8,
-			fill => '#ffffff',
-			text => strftime('%Y-%m-%d %H:%M:%S',localtime),
-			x => 85,
-			y => 10,
-		);
+		# Add caption message
+		$image->Annotate(
+				font      => $font,
+				pointsize => 8,
+				align     => 'right',
+				fill      => CAPTION_COLOUR,
+				text      => uc($msg),
+				x         => $width-6,
+				y         => 10,
+			) if defined $msg && $msg =~ /\S+/;
+	}
 
-	$image->Annotate(
-			font => '/home/nicolaw/bin/Silkscreen.ttf',
-			pointsize => 8,
-			align => 'right',
-			fill => '#ffffff',
-			text => uc($msg),
-			x => $width-6,
-			y => 10,
-		);
-
-	my $overlay = Image::Magick->new;
-#	my @overlay_files = glob('/home/nicolaw/bin/80x15/*.png');
-#	my $overlay_file = $overlay_files[int(rand(@overlay_files))];
-	my $overlay_file = '/home/nicolaw/bin/cam.png';
-	$overlay->ReadImage($overlay_file);
-	$overlay->Set(type => "TrueColorMatte");
-	$image->Composite(
-			compose => 'over',
-			image => $overlay,
-			x => 0, y => 0,
-			opacity => 50,
-		);
+	# Add image overlay
+	my $overlay_file = "$FindBin::Bin/".OVERLAY_IMAGE;
+	if (defined OVERLAY_IMAGE && -f $overlay_file) {
+		my $overlay = Image::Magick->new;
+		$overlay->ReadImage($overlay_file);
+		$overlay->Set(type => "TrueColorMatte");
+		$image->Composite(
+				compose => 'over',
+				image   => $overlay,
+				x       => 0,
+				y       => 0,
+				opacity => 50,
+			);
+	}
 
 	return $image->ImageToBlob;
 }
+
 
 sub SendImage {
 	my ($sub,$opts) = @_;
 	require SOAP::Lite;
 
-	my $port = $opts->{p} || 8021;
-	my $server = $opts->{s} || 'www.neechi.co.uk';
-	my $key = $opts->{k} || 'ec2b5a007a8d0431a36ecadc815e9d82';
-	my $msg = $opts->{m} || '';
-
+	# TODO
+	# Need to change this so that the client and server send and
+	# recieve properly named key/value pairs to aid debugging and
+	# readability of the code.
 	my $soap = SOAP::Lite->new(
-			uri => "http://$server/WebCam/Server/",
-			proxy => "http://$server:$port",
+			uri => "http://$opts->{s}/WebCam/Server/",
+			proxy => "http://$opts->{s}:$opts->{p}",
 		);
 
 	while (my $imgbin = $imgq->dequeue) {
 		eval {
-			my $results = $soap->store_image($key,$imgbin,$msg)->result || {};
+			my $results = $soap->store_image($opts->{k},$imgbin,$opts->{m})->result || {};
 			$viewers = $results->{sleep} > 1 ? 0 : 1;
 			print $results->{bytes_written} ? '>' : '}';
 			sleep $results->{sleep} if
@@ -164,6 +236,8 @@ sub SendImage {
 	}
 }
 
+
 __END__
+
 
 
